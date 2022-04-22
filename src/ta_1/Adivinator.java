29fcdfdc9b34d68ae9e4d259808960a603ae2fe5
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-22 16:42:08 +0200

    cambiado a OOP

---
+++ src/ta_1/Adivinator.java
@@ -0,0 +1,112 @@
+package ta_1;
+
+import java.util.Scanner;
+
+public class Adivinator {
+
+	private static Scanner scnr = new Scanner(System.in);
+	// Attibuts
+	private int inputNr;
+	private int guessNr;
+	private int intentosNr;
+	private boolean adivinado;
+
+	// Constructor
+	public Adivinator() {
+		this.guessNr = generarNumero();
+		this.inputNr = -1;
+		this.intentosNr = 0;
+		this.adivinado = false;
+	}
+
+	// Getters Setters
+
+	/**
+	 * @return the intentosNr
+	 */
+	public int getIntentosNr() {
+		return intentosNr;
+	}
+
+	/**
+	 * @param intentosNr the intentosNr to set
+	 */
+	public void setIntentosNr(int intentosNr) {
+		this.intentosNr = intentosNr;
+	}
+
+	/**
+	 * @return the adivinado
+	 */
+	public boolean isAdivinado() {
+		return adivinado;
+	}
+
+	/**
+	 * @param adivinado the adivinado to set
+	 */
+	public void setAdivinado(boolean adivinado) {
+		this.adivinado = adivinado;
+	}
+
+	// Methods
+	/**
+	 * Pide input de numero entero Devuelve el numero introducido si es un integro
+	 * 
+	 */
+	public void pedirNumero() {
+		int nr = -1;
+		System.out.println("\n Intrduce un número:");
+
+		try {
+			nr = Integer.parseInt(scnr.next());
+		} catch (Exception e) {
+			System.out.println("El valor introducido no se reconoce.");
+		}
+
+		this.inputNr = nr;
+
+	}
+
+	/**
+	 * Genera un numero del 1 al 500
+	 * 
+	 * @return
+	 */
+	public static int generarNumero() {
+
+		return Helpers.random(1, 500);
+	}
+
+	/**
+	 * Mostrar si el numero es mayor o menor. Si se adivina el número se le muestra
+	 * al usuario
+	 * 
+	 * @param inputNr
+	 * @param guessNr
+	 */
+	public void comprobarNumero() {
+
+		if (this.inputNr < this.guessNr) {
+			System.out.println("El numero que buscas es -Mayor-");
+			this.adivinado = false;
+		} else if (this.inputNr > this.guessNr) {
+			System.out.println("El numero que buscas es -menor-");
+			this.adivinado = false;
+		} else {
+			System.out.println("Has adivinado! El número es '" + this.guessNr + "'. Felicidades!");
+			this.adivinado = true;
+		}
+
+	}
+
+	public void sumarIntento() {
+		this.intentosNr++;
+	}
+	
+	public void mostrarIntro() {
+		System.out.println("¡Adivina el número!");
+		System.out.println("Introduce un número entre 1 y 500, ambos incluidos."
+				+ "\n¡Cada intento cuenta! Suerte!");
+	}
+}
--- src/ta_1/unoAdivinarApp.java
+++ src/ta_1/unoAdivinarApp.java
@@ -4,34 +4,33 @@ public class unoAdivinarApp {
 
 	public static void main(String[] args) {
 		
-		int nrIntentos = 0;
-
-		boolean adivinado = false;
+//		Instanciar objeto adivinator
 		
+		Adivinator adv = new Adivinator();
+				
 		// Mostrar por pantalla las instrucciones
-		System.out.println("Tienes que adivinar el número.");
-		System.out.println("Introduce un número entre 1 y 500, ambos incluidos."
-				+ "\n¡Cada intento cuenta! Suerte!");
+		adv.mostrarIntro();
+//		System.out.println("Tienes que adivinar el número.");
+//		System.out.println("Introduce un número entre 1 y 500, ambos incluidos."
+//				+ "\n¡Cada intento cuenta! Suerte!");
 
-		// Generar numero entre 1-500
-		int numeroAdivinar = Controller.generarNumero();
 
 		do {
 			// Pedir al usuario introducir un numero por pantalla
-			int inputNr = Controller.pedirNumero();
-
+			
+			adv.pedirNumero();
+			
 			/*
-			 * Mostrar si el numero es mayor o menor Devuelve false si no has adivinado el
-			 * numero. Devuelve true si se ha adivinado
+			 * Muestra si el número es menor, mayor o correcto.	
 			 */
-			adivinado = Controller.comprobarNumero(inputNr, numeroAdivinar);
+			adv.comprobarNumero();
 
 			// Sumar el número de intentos y mostrarlo por consola
-			nrIntentos++;			
-			System.out.println("Intentos: " + nrIntentos);
+			adv.sumarIntento();
+			System.out.println("Intentos: " + adv.getIntentosNr());
 			
 			// Jugar mientras el número no se haya adivinado
-		} while (!adivinado);
+		} while (!adv.isAdivinado());
 		
 
 	}
