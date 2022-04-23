3fab3ace91d5ea6f358b5de6a134e7f0b38f10e0
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-23 13:35:34 +0200

    empezado punto 4

---
+++ src/ta_4/Matematicas.java
@@ -0,0 +1,20 @@
+package ta_4;
+
+public class Matematicas {
+	
+	// Attributes
+	
+	public Matematicas() {
+	}
+	
+	/**
+	 * Suma
+	 * @param addend1
+	 * @param addend2
+	 * @return
+	 */
+	public double suma(double addend1, double addend2) {
+		return addend1 + addend2;
+	}
+
+}
---
+++ src/ta_4/mainCuatroApp.java
@@ -0,0 +1,12 @@
+package ta_4;
+
+public class mainCuatroApp {
+
+	public static void main(String[] args) {
+		
+		Matematicas matem = new Matematicas();
+		
+
+	}
+
+}
